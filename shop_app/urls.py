38bc69f9896ab9a98556c00e14223e46dd463565
Kirugik <kirugik79@gmail.com>
2022-10-03 15:56:06 +0300

    feat: Update Admin, Clerk, Order, and Defective product views

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -9,10 +9,33 @@ urlpatterns = [
     path('update-category/<int:pk>/', views.update_category, name='update-category'), 
     path('remove-category/<int:pk>/', views.remove_category, name='remove-category'),
 
-
     path('products/', views.product_list, name='product-list'), 
     path('new-product', views.new_product, name='new-product'),
     path('product-info/<int:pk>', views.product_info, name='product-info'),
     path('update-product/<int:pk>/', views.update_product_info, name='update-product'), 
     path('remove-product/<int:pk>/', views.remove_product, name='remove-product'),
+
+    path('orders/', views.current_orders, name='orders'),
+    path('create-order/', views.create_order, name='create-order'),
+    path('order-info/<int:pk>/', views.order_info, name='order-info'),
+    path('update-order/<int:pk>/', views.update_order_info, name='update-order'),
+    path('remove-order/<int:pk>/', views.remove_order, name='remove-order'),
+
+    path('defective-products/', views.defective_products_list, name='defective-products'),
+    path('create-defective-product/', views.create_defective_product, name='create-defective-product'),
+    path('defective-product-info/<int:pk>/', views.defective_product_info, name='defective-product-info'),
+    path('update-defective-product/<int:pk>/', views.update_defective_product, name='update-defective-product'),
+    path('remove-defective-product/<int:pk>/', views.remove_defective_product, name='remove-defective-product'),
+
+    path('clerks/', views.all_clerks, name='clerks'), 
+    path('add-clerk/', views.add_clerk, name='add-clerk'), 
+    path('clerk-details/<int:pk>/', views.clerk_details, name='clerk-details'),
+    path('update-clerk/<int:pk>/', views.update_clerk_details, name='update-clerk'),
+    path('remove-clerk/<int:pk>/', views.remove_clerk, name='remove-clerk'),  
+    
+    path('admins/', views.all_admins, name='admins'), 
+    path('add-admin/', views.add_admin, name='add-admin'), 
+    path('admin-details/<int:pk>/', views.admin_details, name='admin-details'),
+    path('update-admin/<int:pk>/', views.update_admin_details, name='update-admin'), 
+    path('remove-admin/<int:pk>/', views.remove_admin, name='remove-admin'),
 ] 
--- shop_app/views.py
+++ shop_app/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import CategorySerializer, ProductSerializer, AdminSerializer, ClerkSerializer
-from .models import Category, Product, Clerk, Admin
+from .serializers import CategorySerializer, ProductSerializer, AdminSerializer, ClerkSerializer, OrderSerializer, DefectiveProductSerializer
+from .models import Category, Product, Clerk, Admin, Order, DefectiveProduct
 # from shop_app import serializers
 
 # Create your views here.
@@ -49,7 +49,6 @@ def remove_category(request, pk):
 
 
 
-
 # products api
 @api_view(['GET'])
 def product_list(request):
@@ -87,6 +86,80 @@ def remove_product(request, pk):
 
 
 
+# orders api
+@api_view(['GET'])
+def current_orders(request):
+    orders = Order.objects.all().order_by('-id')
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_order(request):
+    serializer = OrderSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def order_info(request, pk):
+    order = Order.objects.get(id=pk)
+    serializer = OrderSerializer(order, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_order_info(request, pk):
+    order = Order.objects.get(id=pk)
+    serializer = OrderSerializer(instance=order, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data) 
+
+@api_view(['DELETE'])
+def remove_order(request, pk):
+    order = Order.objects.get(id=pk)
+    order.delete()
+    return Response("Order deleted successfully!")
+
+
+
+
+# defective products api
+@api_view(['GET'])
+def defective_products_list(request):
+    defective_products = DefectiveProduct.objects.all().order_by('-id')
+    serializer = DefectiveProductSerializer(defective_products, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_defective_product(request):
+    serializer = DefectiveProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def defective_product_info(request, pk):
+    defective_product = DefectiveProduct.objects.get(id=pk)
+    serializer = DefectiveProductSerializer(defective_product, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_defective_product(request, pk):
+    defective_product = DefectiveProduct.objects.get(id=pk)
+    serializer = ProductSerializer(instance=defective_product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data) 
+
+@api_view(['DELETE'])
+def remove_defective_product(request, pk):
+    defective_product = DefectiveProduct.objects.get(id=pk)
+    defective_product.delete()
+    return Response("Defective product deleted successfully!")
+
+
+
+
 # clerks api
 @api_view(['GET'])
 def all_clerks(request):
@@ -100,6 +173,26 @@ def add_clerk(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET'])
+def clerk_details(request, pk):
+    clerk = Clerk.objects.get(id=pk)
+    serializer = ClerkSerializer(clerk, many=False)
+    return Response(serializer.data) 
+
+@api_view(['PUT'])
+def update_clerk_details(request, pk):
+    clerk = Clerk.objects.get(id=pk)
+    serializer = ClerkSerializer(instance=clerk, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)  
+
+@api_view(['DELETE'])
+def remove_clerk(request, pk):
+    clerk = Clerk.objects.get(id=pk)
+    clerk.delete()
+    return Response("Clerk removed successfully!")
 
 
 
@@ -117,3 +210,27 @@ def add_admin(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['GET'])
+def admin_details(request, pk):
+    admin = Admin.objects.get(id=pk)
+    serializer = AdminSerializer(admin, many=False)
+    return Response(serializer.data) 
+
+@api_view(['PUT'])
+def update_admin_details(request, pk):
+    admin = Admin.objects.get(id=pk)
+    serializer = AdminSerializer(instance=admin, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data) 
+
+@api_view(['DELETE'])
+def remove_admin(request, pk):
+    admin = Admin.objects.get(id=pk)
+    admin.delete()
+    return Response("Admin removed successfully!")
+
+
+
+
